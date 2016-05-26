7a42b8539a7f879f42563808636892261ee47bf9
Andris Birza <andris2.birza@live.uwe.ac.uk>
2016-05-26 11:19:19 +0100

    added functions to typing.js

--- app/assets/js/typing.js
+++ app/assets/js/typing.js
@@ -2,7 +2,7 @@ const MAX_FRAGMENTS = 32;
 
 var fragments_list = [];
 var fragment_count = 0;
-var current_fragment = 0;
+var cursor = 0;
 
 function print_fragments()
 {
@@ -18,13 +18,13 @@ function update_fragments(text)
     fragments_list.length = 0;
     for(var i = 0; i < MAX_FRAGMENTS, i < text.length; i++)
     {
-        console.log("tick");
+        //console.log("tick");
         var fragment = document.createElement("span");
         fragment.textContent = text[i];
         fragments_list.push(fragment);
     }
 
-    print_fragments();
+    //print_fragments();
 }
 
 function display_fragments()
@@ -32,7 +32,7 @@ function display_fragments()
     var fragments_div = document.getElementById("fragments_div");
     while(fragments_div.firstChild)
     {
-        console.log("remove child span");
+        //console.log("remove child span");
         fragments_div.removeChild(fragments_div.firstChild);
     }
 
@@ -42,10 +42,47 @@ function display_fragments()
     }
 }
 
+function select_fragment(index)
+{
+    if(index < fragments_list.length)
+    {
+        var fragment = fragments_list[index];
+        if(fragment != null)
+        {
+            fragment.style.fontSize = "96px";
+            fragment.style.color = "red";
+            fragment.style.textDecoration = "underline";
+        }
+    }
+}
+
+function unselect_fragment(index)
+{
+    if(index < fragments_list.length)
+    {
+        var fragment = fragments_list[index];
+        if(fragment != null)
+        {
+            fragment.style.fontSize = "72px";
+            fragment.style.color = "black";
+            fragment.style.textDecoration = "none";
+        }
+    }
+}
+
 function move_cursor()
 {
-    var frags = document.getElementById("fragments_div").childNodes;
+    unselect_fragment(cursor);
+    cursor++;
+    select_fragment(cursor);
+}
 
+function handle_keypress(event)
+{
+    var key = event.keyCode || event.which;
+
+    console.log("keypress ...." + key);
+    move_cursor();
 }
 
 (function start()
@@ -54,7 +91,9 @@ function move_cursor()
 
     var text = "sample text";
 
-
+    update_fragments(text);
+    display_fragments();
+    select_fragment(cursor);
     //init_fragments();
     //set_fragments(text);
 
@@ -78,4 +117,5 @@ function move_cursor()
     //console.log(frags_array[1]);
     //frags_array[1].style.fontSize = "48px";
     //frags_array[1].style.color = "red";
+    var body = document.body.addEventListener("keypress", handle_keypress);
 })();
